42edc6002113a9dbefe84e10960fc0441766f0c9
Piyush Kacha <pkacha@gmail.com>
2025-09-05 11:37:11 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(policy =>
+{
+    policy.AllowAnyHeader().AllowAnyMethod()
+        .WithOrigins("http://localhost:4200", "https://localhost:4200");
+});
+
 app.MapControllers();
 
 app.Run();
