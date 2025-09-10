4d03bb9b3699034a577d8b039613130150ada7d8
Piyush Kacha <pkacha@gmail.com>
2025-09-10 12:01:10 +0530

    End of section 5

---
+++ client/src/core/services/account-service.ts
@@ -0,0 +1,45 @@
+import { LoginCreds, RegisterCreds } from './../../types/user';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../../types/user';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  currentUser = signal<User | null>(null);
+
+  baseUrl = 'https://localhost:5001/api/';
+
+  regiser(creds: RegisterCreds) {
+    return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
+      tap((user) => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
+  login(creds: LoginCreds) {
+    return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
+      tap((user) => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUser.set(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+}
