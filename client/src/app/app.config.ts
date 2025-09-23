9c2a712997bed26f175a86d21fcb7eb9fe16f73b
Piyush Kacha <pkacha@gmail.com>
2025-09-23 11:48:59 +0530

    End of section 9

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -12,6 +12,7 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { InitService } from '../core/services/init-service';
 import { lastValueFrom } from 'rxjs';
 import { errorInterceptor } from '../core/interceptors/error-interceptor';
+import { jwtInterceptor } from '../core/interceptors/jwt-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,7 +20,7 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes, withViewTransitions()),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor])),
     provideAppInitializer(async () => {
       const initService = inject(InitService);
 
--- client/src/features/member/member-detailed/member-detailed.ts
+++ client/src/features/member/member-detailed/member-detailed.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
+import { filter, Observable } from 'rxjs';
+import { Member } from '../../../types/member';
+import { AgePipe } from '../../../core/pipes/age-pipe';
 
 @Component({
   selector: 'app-member-detailed',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, AgePipe],
   templateUrl: './member-detailed.html',
-  styleUrl: './member-detailed.css'
+  styleUrl: './member-detailed.css',
 })
-export class MemberDetailed {
+export class MemberDetailed implements OnInit {
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  protected member = signal<Member | undefined>(undefined);
+  protected title = signal<string | undefined>('Profile');
 
+  ngOnInit(): void {
+    this.route.data.subscribe({
+      next: (data) => this.member.set(data['member']),
+    });
+    this.title.set(this.route.firstChild?.snapshot?.title);
+
+    this.router.events.pipe(filter((event) => event instanceof RouterLinkActive)).subscribe({
+      next: () => {
+        this.title.set(this.route.firstChild?.snapshot?.title);
+      },
+    });
+  }
 }
